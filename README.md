fa37a4dd5de066b019c405269edad6ecbc255d3b
maxineafable <maxineafable@yahoo.com>
2026-08-07 19:21:20 +0800

    feat: add file category support via --type flag

--- README.md
+++ README.md
@@ -8,10 +8,16 @@ Maliit is a simple CLI file organizer written in Rust.
 
 Organize all images in the source directory
 ```
-cargo run -- images ~/src-path ~/dest-path
+cargo run -- organize ~/src-path ~/dest-path -t image
 ```
 
 If specifying file extensions
 ```
-cargo run -- images ~/src-path ~/dest-path --ext jpg png
+cargo run -- organize ~/src-path ~/dest-path -t image -e jpg png
 ```
+
+## Flags
+
+* `-t, --type` — Filter by file category (`docs`, `image`).
+* `-e, --ext` — Filter by file extensions (e.g., `jpg png`).
+* `-o, --ovr` — Overwrite duplicate files in the destination directory.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, io::Write, path::PathBuf};
 
-use clap::{Parser, Subcommand};
+use clap::{Parser, Subcommand, ValueEnum};
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -10,14 +10,27 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    Images {
+    Organize {
         path_from: PathBuf,
         path_to: PathBuf,
+
+        #[arg(short = 't', long = "type", value_enum)]
+        file_type: Option<FileType>,
+        
+        /// Only organize files with the specified extensions (e.g. jpg, png).
         #[arg(short = 'e', long = "ext", value_delimiter = ' ', num_args = 1..)]
         extensions: Vec<String>,
+
+        /// Overwrite existing files in dest directory
         #[arg(short = 'o', long = "ovr")]
         overwrite: bool,
     },
+}
+
+#[derive(Debug, Clone, ValueEnum)]
+enum FileType {
+    Docs,
+    Image,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -26,66 +39,74 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let image_files = vec!["jpg", "png", "gif", "jpeg", "webp"];
 
     match &args.command {
-        Commands::Images {
+        Commands::Organize {
             path_from,
             path_to,
-            overwrite,
+            file_type,
             extensions,
-        } => {
-            let entries = fs::read_dir(path_from)?;
-            
-            for entry in entries {
-                let entry = entry?;
-                let path = entry.path();
+            overwrite,
+        } => match file_type {
+            Some(FileType::Image) => {
+                let entries = fs::read_dir(path_from)?;
 
-                if !path.is_file() {
-                    continue;
-                }
+                for entry in entries {
+                    let entry = entry?;
+                    let path = entry.path();
 
-                let path_ext = path.extension().and_then(|e| e.to_str());
+                    if !path.is_file() {
+                        continue;
+                    }
 
-                let allowed_img = path_ext.map_or(false, |ext| {
-                    image_files.contains(&ext)
-                        && (extensions.is_empty() || extensions.iter().any(|e| e == ext))
-                });
+                    let path_ext = path.extension().and_then(|e| e.to_str());
 
-                let dst_path = path_to.join(path.file_name().unwrap());
+                    let allowed_img = path_ext.map_or(false, |ext| {
+                        image_files.contains(&ext)
+                            && (extensions.is_empty() || extensions.iter().any(|e| e == ext))
+                    });
 
-                if allowed_img {
-                    if !overwrite && dst_path.exists() {
-                        let filename = path
-                            .file_name()
-                            .and_then(|f| f.to_str())
-                            .unwrap_or("unknown file");
-                        loop {
-                            print!("File: {} already exists. Overwrite? (Y/n): ", filename);
-                            std::io::stdout().flush().unwrap();
+                    let dst_path = path_to.join(path.file_name().unwrap());
 
-                            let mut input: String = String::new();
-                            std::io::stdin()
-                                .read_line(&mut input)
-                                .expect("Failed to read line");
-                            let choice = input.trim().to_lowercase();
-                            match choice.as_str() {
-                                "y" | "yes" => {
-                                    fs::create_dir_all(&path_to)?;
-                                    fs::rename(&path, &dst_path)?;
+                    if allowed_img {
+                        if !overwrite && dst_path.exists() {
+                            let filename = path
+                                .file_name()
+                                .and_then(|f| f.to_str())
+                                .unwrap_or("unknown file");
+                            loop {
+                                print!("File: {} already exists. Overwrite? (Y/n): ", filename);
+                                std::io::stdout().flush().unwrap();
+
+                                let mut input: String = String::new();
+                                std::io::stdin()
+                                    .read_line(&mut input)
+                                    .expect("Failed to read line");
+                                let choice = input.trim().to_lowercase();
+                                match choice.as_str() {
+                                    "y" | "yes" => {
+                                        fs::create_dir_all(&path_to)?;
+                                        fs::rename(&path, &dst_path)?;
+                                    }
+                                    "n" | "no" => {
+                                        println!("Skipping file {}", filename);
+                                        break;
+                                    }
+                                    _ => println!("Invalid input"),
                                 }
-                                "n" | "no" => {
-                                    println!("Skipping file {}", filename);
-                                    break;
-                                }
-                                _ => println!("Invalid input"),
                             }
+                        } else {
+                            fs::create_dir_all(&path_to)?;
+                            fs::rename(&path, dst_path)?;
                         }
-                    } else {
-                        fs::create_dir_all(&path_to)?;
-                        fs::rename(&path, dst_path)?;
                     }
                 }
-            }
 
-            Ok(())
-        }
+                Ok(())
+            }
+            Some(FileType::Docs) => {
+                Ok(())
+            }
+            // TODO: handle if no specified file type, maybe organize all files in src dir
+            None => Err("Invalid file type".into()),
+        },
     }
 }
